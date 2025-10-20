62b4aba6fd1c645f69a2f91760e3afd56b686543
Ty Ochsen <ty@Tys-MacBook-Pro.lan>
2025-10-19 21:07:03 -0400

    Update Firebase config to DaemonCore project

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,10 +1,11 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCE1F7p_6tcJ8U7pYaVbbR7xC2-rNKnqPs",
   authDomain: "nexus-a317c.firebaseapp.com",
   projectId: "nexus-a317c",
-  storageBucket: "nexus-a317c.appspot.com",
+  storageBucket: "nexus-a317c.firebasestorage.app",
   messagingSenderId: "127427301669",
   appId: "1:127427301669:web:1c28c82f832a933fdd0acc",
   measurementId: "G-NZ2C6LGR1Y"
